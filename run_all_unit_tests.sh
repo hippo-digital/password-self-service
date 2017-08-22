134ee4dd5206620b6e5c6f3e6d4ffce310611ba7
Brett Jackson <brett@hippodigital.co.uk>
2017-08-22 16:16:00 +0100

    Scripted testing bits

--- run_all_unit_tests.sh
+++ run_all_unit_tests.sh
@@ -5,11 +5,13 @@ RES=0
 START_PATH=${PWD}
 
 cd $START_PATH/password_reset_frontend_receiver/
+source bin/activate
 python3 -m unittest receiver/tests_receiver.py
 RES=$(( $? + $RES ))
 echo $RES
 
 cd $START_PATH/password_reset_frontend_ui/
+source bin/activate
 python3 -m unittest ui/tests_ui.py
 RES=$(( $? + $RES ))
 echo $RES
