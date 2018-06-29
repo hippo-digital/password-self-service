9a09d3a93ab27eb3cdc247943134b89471faaf56
Brett Jackson <github@bcjmail.co.uk>
2018-06-29 12:42:44 +0100

    Rotating file handler

--- password_reset_frontend_ui/server.py
+++ password_reset_frontend_ui/server.py
@@ -1,12 +1,12 @@
 from ui import app
-import logging
+import logging, logging.handlers
 
 app.secret_key = 'Secret123!'
 app.config['SESSION_TYPE'] = 'filesystem'
 
 log = logging.getLogger('password_reset_frontend')
 log.setLevel(logging.DEBUG)
-fh = logging.FileHandler('password_reset_frontend.log')
+fh = logging.handlers.RotatingFileHandler('password_reset_frontend.log', maxBytes=(32 * 1048576), backupCount=3)
 fh.setLevel(logging.DEBUG)
 log.addHandler(fh)
 formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
